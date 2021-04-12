39a28849b061bfca51869594b651b1d49bdedaa1
Anton <cminskii5@gmail.com>
2021-04-12 11:03:29 +0300

    add new interface

--- js/app.js
+++ js/app.js
@@ -1,15 +1,38 @@
-function accordionOpen(elem,openIMG,closeIMG) {
+function accordionAction(elem, openIMG, closeIMG) {
+    const currentAccDescrip = elem.parentNode.lastElementChild;
+    const accordionInterf = document.querySelector("input[type='radio']:checked")
 
+    console.log(accordionInterf.value)
 
-    document.querySelectorAll(".accordion__description").forEach(element => {
-        element.classList.remove("accordion__description_open")
-        element.classList.add("accordion__description_close")
+    if (accordionInterf.value == "one") {
 
-        element.parentNode.firstElementChild.lastElementChild.setAttribute("src", openIMG)
+        if (currentAccDescrip.classList.contains("accordion__description_close")) {
 
-    })
+            currentAccDescrip.classList.add("accordion__description_open");
 
-    elem.parentNode.lastElementChild.classList.add("accordion__description_open");
-    elem.parentNode.lastElementChild.classList.remove("accordion__description_close");
-    elem.parentNode.firstElementChild.lastElementChild.setAttribute("src", closeIMG)
+            currentAccDescrip.classList.remove("accordion__description_close");
+
+        } else {
+
+            currentAccDescrip.classList.add("accordion__description_close");
+
+            currentAccDescrip.classList.remove("accordion__description_open");
+        }
+
+    } else {
+
+        console.log("sdf")
+        document.querySelectorAll(".accordion__description").forEach(element => {
+
+            element.classList.remove("accordion__description_open")
+            element.classList.add("accordion__description_close")
+
+            element.parentNode.firstElementChild.lastElementChild.setAttribute("src", openIMG)
+
+        })
+
+        currentAccDescrip.classList.add("accordion__description_open");
+        currentAccDescrip.classList.remove("accordion__description_close");
+        currentAccDescrip.lastElementChild.setAttribute("src", closeIMG)
+    }
 }
