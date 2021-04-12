fc348ea0985b7026a4c51a90c1336e2f6866dde6
Anton <cminskii5@gmail.com>
2021-04-12 11:14:41 +0300

    fix img

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 function accordionAction(elem, openIMG, closeIMG) {
-    
+
     const currentAccDescrip = elem.parentNode.lastElementChild;
     const accordionInterf = document.querySelector("input[type='radio']:checked")
 
-    console.log(accordionInterf.value)
 
     if (accordionInterf.value == "one") {
 
@@ -13,11 +12,15 @@ function accordionAction(elem, openIMG, closeIMG) {
 
             currentAccDescrip.classList.remove("accordion__description_close");
 
+            currentAccDescrip.parentNode.firstElementChild.lastElementChild.setAttribute("src", closeIMG)
+
         } else {
 
             currentAccDescrip.classList.add("accordion__description_close");
 
             currentAccDescrip.classList.remove("accordion__description_open");
+
+            currentAccDescrip.parentNode.firstElementChild.lastElementChild.setAttribute("src", openIMG)
         }
 
     } else {
@@ -33,6 +36,6 @@ function accordionAction(elem, openIMG, closeIMG) {
 
         currentAccDescrip.classList.add("accordion__description_open");
         currentAccDescrip.classList.remove("accordion__description_close");
-        currentAccDescrip.lastElementChild.setAttribute("src", closeIMG)
+        currentAccDescrip.parentNode.firstElementChild.lastElementChild.setAttribute("src", closeIMG)
     }
 }
